da1d52712d4898724565488f4fe4825e28776723
Muhammad Rafie <rafietriyanto08@gmail.com>
2022-08-21 11:14:48 +0700

    Improve setMap() system

--- 3drc.js
+++ 3drc.js
@@ -1,6 +1,11 @@
-const tdrc={};
+const tdrc={map:{}};
 
-tdrc.setMap=(data,width,height)=>{
+tdrc._assign=(target,obj)=>{
+  Object.entries(obj).forEach(v=>tdrc[target][v[0]]=v[1]);
+}
+
+tdrc.setMap=map=>{
+  tdrc._assign('map',map);
+  let {data,width,height}=tdrc.map;
   if(data.length<width*height)throw new Error('Map data is shorter than map size');
-  tdrc.map={data,width,height};
 }
