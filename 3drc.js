c170b65ec20a449415f42bf32e22b0030d0a9f96
Muhammad Rafie <rafietriyanto08@gmail.com>
2022-08-28 21:18:13 +0700

    Add player object and render preparation

--- 3drc.js
+++ 3drc.js
@@ -1,4 +1,9 @@
-const tdrc={map:{},graphic:{}};
+const tdrc={
+  map:{cellSize:5},
+  player:{},
+  graphic:{},
+  _rays:0
+};
 
 
 tdrc._assign=(target,obj)=>Object.entries(obj).forEach(v=>tdrc[target][v[0]]=v[1]);
@@ -6,6 +11,14 @@ tdrc._validate=(obj,list,name)=>{
   list.forEach(v=>{if(!obj.hasOwnProperty(v))throw new Error(`${name.charAt(0).toUpperCase()+name.slice(1)} must has "${v}"`)});
 }
 tdrc._toRadian=degree=>degree*Math.PI/180;
+
+tdrc.player={
+  x: 1,
+  y: 1,
+  size: 0.2*tdrc.map.cellSize,
+  angle: tdrc._toRadian(45),
+  speed: 0.05
+}
 
 tdrc.graphic={
   totalRay: 300,
@@ -26,4 +39,11 @@ tdrc.setGraphic=graphic=>{
   if(graphic.hasOwnProperty('fov'))graphic.fov=tdrc._toRadian(graphic.fov);
   tdrc._assign('graphic',graphic);
   let {totalRay,fov,rayStep,texture,depth}=tdrc.graphic;
+}
+
+tdrc.render=canvas=>{
+  const ctx=canvas.getContext('2d');
+  ctx.clearRect(0,0,canvas.width,canvas.height);
+  tdrc._rays=tdrc._castRay();
+  tdrc._render3d(tdrc._rays);
 }
