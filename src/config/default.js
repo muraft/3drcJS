62af6291e894fdd31f90d0884ce7691e5e413be8
Muhammad Rafie <rafietriyanto08@gmail.com>
2023-06-22 17:54:53 +0700

    Still preparing and messing up wit webpack

---
+++ src/config/default.js
@@ -0,0 +1,28 @@
+var path = require('path');
+const {toRadian}=require(path.resolve(__dirname,"../modules/utils.js"));
+
+const mapCellSize=5;
+module.exports={
+    map:{
+      cellSize:mapCellSize
+    },
+    texture:{
+      colour:'blue', 
+      data: {}
+    },
+    player:{
+      x: 1,
+      y: 1,
+      size: 0.2*mapCellSize,
+      angle: toRadian(45),
+      speed: 0.05
+    },
+    graphic:{
+      totalRay: 300,
+      fov: toRadian(45),
+      rayStep: 0.05,
+      texture: true,
+      depth: false
+    },
+    rays:0
+}
