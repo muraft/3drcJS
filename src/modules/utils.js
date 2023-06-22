bc2a80a17b3aee345d344d26fa05760df55c0662
coolraptor08 <rafietriyanto08@gmail.com>
2023-06-22 20:38:37 +0700

    finished modules loader

--- src/modules/utils.js
+++ src/modules/utils.js
@@ -1,3 +1,17 @@
 const toRadian=(degree)=>degree*Math.PI/180;
 
-module.exports={toRadian}
+module.exports={
+  toRadian:degree=>degree*Math.PI/180,
+
+  assign:(target,obj)=>Object.entries(obj).forEach(v=>this[target][v[0]]=v[1]),
+
+  validate:(obj,list,name)=>{
+    list.forEach(v=>{
+      if(!obj.hasOwnProperty(v)){
+        throw new Error(`${name.charAt(0).toUpperCase()+name.slice(1)} must has "${v}"`)
+      }
+    });
+  },
+
+  toIndex:(x,y)=>y*this.map.side+x
+}
