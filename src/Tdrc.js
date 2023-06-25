8b45eeffae1ba641a48cb2eb421ff4113c9ac739
Muhammad Rafie <rafietriyanto08@gmail.com>
2023-06-25 19:06:24 +0700

    Buttons class to help user to shorten code

--- src/Tdrc.js
+++ src/Tdrc.js
@@ -1,14 +1,17 @@
-const {loadDefault,loadMethods}=require('./loader.js');
+const {loadDefault,loadMethods,loadClasses}=require('./loader.js');
 const loadBlock=require('./modules/loadBlock.js');
 
 class Tdrc{
   constructor(){
     loadDefault(this);
-    loadMethods(this);
+    loadMethods(this, ['set','control','render']);
   }
   init(){
     loadBlock(this);
   }
 }
+
+if(typeof window!='undefined')loadClasses(['Buttons']);
+else console.warn('\x1b[33m%s\x1b[0m' ,"Application runned outside the browser, window is not found, the global class won't load");
 
 module.exports=Tdrc;
