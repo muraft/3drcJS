fcff074a7a7daa5d36fcadb6f671a43584cbf738
Muhammad Rafie <rafietriyanto08@gmail.com>
2023-06-27 10:39:03 +0700

    drawMap finished

--- src/methods/drawMap.js
+++ src/methods/drawMap.js
@@ -1,15 +1,29 @@
-const drawMap=function(x,y,width,canvas){
+const drawMap=function(x,y,scale,canvas){
   const ctx=canvas.getContext('2d');
+  
   this.map.data.forEach((v,i)=>{
-    xPos=(x+i%this.map.width)*width; 
-    yPos=Math.floor(y+i/this.map.width)*width;
+    xPos=(x+i%this.map.width)*scale; 
+    yPos=Math.floor(y+i/this.map.width)*scale;
     
     ctx.fillStyle=v==0?'grey':'blue';
-    ctx.fillRect(xPos,yPos,width,width)
+    ctx.fillRect(xPos,yPos,scale,scale)
   })
+  
   ctx.fillStyle='red';
-  ctx.arc(this.player.x*width,this.player.y*width,2,0,2*Math.PI);
+  ctx.beginPath();
+  ctx.arc(this.player.x*scale,this.player.y*scale,2,0,2*Math.PI);
   ctx.fill(); 
+  ctx.closePath();
+  
+  this.rays.forEach(ray=>{
+    ctx.strokeWidth=10; 
+    ctx.strokeStyle="yellow"; 
+    ctx.beginPath(); 
+    ctx.moveTo(this.player.x*scale,this.player.y*scale); 
+    ctx.lineTo(ray.distanceX,ray.distanceY); 
+    ctx.stroke(); 
+    ctx.closePath();
+  })
 }
 
 module.exports=drawMap;
